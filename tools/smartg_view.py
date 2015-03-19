e282d8df9452313b8acda7c61ec8d5b3555bf934
François Steinmetz <fs@hygeos.com>
2015-03-19 09:44:45 +0100

    Improve semi_polar
    Now properly handle various custom axes.

--- tools/smartg_view.py
+++ tools/smartg_view.py
@@ -12,7 +12,6 @@ from mpl_toolkits.axisartist import floating_axes
 from matplotlib.projections import PolarAxes
 from mpl_toolkits.axisartist.grid_finder import FixedLocator, DictFormatter
 from luts import Idx
-#from  mpl_toolkits.axisartist.grid_helper_curvelinear import GridHelperCurveLinear
 
 
 
@@ -51,9 +50,11 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
     if swap:
         ax1, ax2 = lut.axes[1], lut.axes[0]
         name1, name2 = lut.names[1], lut.names[0]
+        data = np.swapaxes(lut.data, 0, 1)
     else:
         ax1, ax2 = lut.axes[0], lut.axes[1]
         name1, name2 = lut.names[0], lut.names[1]
+        data = lut.data
 
     if vmin is None:
         vmin = np.amin(lut.data)
@@ -67,46 +68,53 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
     #
     # semi polar axis
     #
+    ax1_ticks = [0, 45, 90, 135, 180]
     if 'azimu' in name1.lower():
-        ax1_min, ax1_max = 0.,180.
-        ax1_ticks = [0, 45, 90, 135, 180]
+        ax1_min, ax1_max = 0., 180.
+        ax1_ticks = dict(zip(ax1_ticks, map(str, ax1_ticks)))
         label1 = r'$\phi$'
+        ax1_scaled = ax1
     else:
         ax1_min, ax1_max = ax1[0], ax1[-1]
-        ax1_ticks = np.linspace(ax1_min, ax1_max, 5)
+        ax1_ticks = dict(zip(ax1_ticks,
+                         map(lambda x: '{:.1f}'.format(x), np.linspace(ax1_min, ax1_max, len(ax1_ticks)))))
         label1 = name1
 
+        # rescale ax1 to (0, 180)
+        ax1_scaled = (ax1-ax1_min)/(ax1_max-ax1_min)*180.
+
+    ax2_ticks = [0, 30, 60, 90]
     if 'zenit' in name2.lower():
         ax2_min, ax2_max = 0, 90.
         if sym is None: sym=True
-        ax2_ticks = [0, 30, 60, 90]
-        if sym: ax2_ticks = [-90, -60, -30, 0, 30, 60, 90]
+        ax2_ticks = dict(zip(ax2_ticks, map(str, ax2_ticks)))
         label2 = r'$\theta$'
+        ax2_scaled = ax2
     else:
         ax2_min, ax2_max = ax2[0], ax2[-1]
-        ax2_ticks = np.linspace(ax2_min, ax2_max, 5)
+        ax2_ticks = dict(zip(ax2_ticks,
+                             map(lambda x: '{:.1f}'.format(x), np.linspace(ax2_min, ax2_max, len(ax2_ticks)))))
         if sym is None: sym=False
-        if sym:
-            ax2_ticks = list(-ax2_ticks[1:][::-1]) + list(ax2_ticks)
         label2 = name2
 
+        # rescale to (0, 90)
+        ax2_scaled = (ax2-ax2_min)/(ax2_max-ax2_min)*90.
+
     # 1st axis
-    grid_locator1 = FixedLocator(ax1_ticks )
-    tick_formatter1 = DictFormatter(dict(zip(ax1_ticks,
-            map(lambda x: r'${}$'.format(x), ax1_ticks))))
+    grid_locator1 = FixedLocator(ax1_ticks.keys())
+    tick_formatter1 = DictFormatter(ax1_ticks)
 
     # 2nd axis
-    grid_locator2 = FixedLocator(ax2_ticks)
-    tick_formatter2 = DictFormatter(dict(zip(ax2_ticks,
-            map(lambda x: r'${}$'.format(x), ax2_ticks))))
+    grid_locator2 = FixedLocator(ax2_ticks.keys())
+    tick_formatter2 = DictFormatter(ax2_ticks)
 
     tr_rotate = Affine2D().translate(0, 0)  # orientation
-    tr_scale = Affine2D().scale(np.pi/ax1_max, 1.)  # scale to radians
+    tr_scale = Affine2D().scale(np.pi/180., 1.)  # scale to radians
 
     tr = tr_rotate + tr_scale + PolarAxes.PolarTransform()
 
     grid_helper = floating_axes.GridHelperCurveLinear(tr,
-                                    extremes=(ax1_min, ax1_max, ax2_min, ax2_max),
+                                    extremes=(0., 180., 0., 90.),
                                     grid_locator1=grid_locator1,
                                     grid_locator2=grid_locator2,
                                     tick_formatter1=tick_formatter1,
@@ -126,9 +134,12 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
     ax_polar.axis["top"].major_ticklabels.set_axis_direction("top")
     ax_polar.axis["top"].label.set_axis_direction("top")
 
-    ax_polar.axis["top"].label.set_text(label1)
+    ax_polar.axis["top"].axes.text(0.70, 0.92, label1,
+                                   transform=ax_polar.transAxes,
+                                   ha='left',
+                                   va='bottom')
     ax_polar.axis["left"].axes.text(0.25, -0.03, label2,
-                                    transform=ax_polar.transAxes,
+                                   transform=ax_polar.transAxes,
                                    ha='center',
                                    va='top')
 
@@ -152,7 +163,6 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
             ax_cart.set_xlim(ax2_min, ax2_max)
         ax_cart.set_ylim(vmin, vmax)
         ax_cart.grid(True)
-        ax_cart.set_xticks(ax2_ticks)
 
     #
     # draw colormesh
@@ -161,23 +171,18 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
     cmap.set_under('black')
     cmap.set_over('white')
     cmap.set_bad('0.5') # grey 50%
-    r, t = np.meshgrid(ax2, ax1)
-    if swap:
-        data = np.swapaxes(lut.data, 0, 1)
-    else:
-        data = lut.data
+    r, t = np.meshgrid(ax2_scaled, ax1_scaled)
     masked_data = np.ma.masked_where(np.isnan(data) | np.isinf(data), data)
     im = aux_ax_polar.pcolormesh(t, r, masked_data, cmap=cmap, vmin=vmin, vmax=vmax)
 
     if show_sub:
-        
         # convert Idx instance to index if necessarry
         if isinstance(index, Idx):
             index = int(round(index.index(ax1)))
 
         # draw line over colormesh
-        vertex0 = np.array([[0,0],[ax1[index],ax2_max]])
-        vertex1 = np.array([[0,0],[ax1[-1-index],ax2_max]])
+        vertex0 = np.array([[0,0],[ax1_scaled[index],ax2_max]])
+        vertex1 = np.array([[0,0],[ax1_scaled[-1-index],ax2_max]])
         aux_ax_polar.plot(vertex0[:,0],vertex0[:,1], 'w')
         if sym:
             aux_ax_polar.plot(vertex1[:,0],vertex1[:,1],'w--')
@@ -190,7 +195,8 @@ def semi_polar(lut, index=None, vmin=None, vmax=None, rect='211', sub='212',
             ax_cart.plot(-ax2, data[-1-index,:],'k--')
 
     # add colorbar
-    cb = fig.colorbar(im, orientation='horizontal', extend='both', ticks=np.linspace(vmin, vmax, 5))
+    fig.colorbar(im, orientation='horizontal', extend='both', ticks=np.linspace(vmin, vmax, 5))
     if lut.desc is not None:
-        cb.set_label(lut.desc)
+        ax_polar.set_title(lut.desc, weight='bold', position=(0.15,0.9))
+
 
