d63f0829b5acbf597f879b4645cff0601b97e476
François Steinmetz <fs@hygeos.com>
2019-10-17 11:14:47 +0200

    Add a very simple test of pycuda

---
+++ tests/test_cuda.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+import pycuda.driver as drv
+import pycuda.tools
+import pycuda.autoinit
+import numpy
+import numpy.linalg as la
+from pycuda.compiler import SourceModule
+
+
+def test_pycuda():
+    '''
+    A basic pycuda test, from pycuda examples hello_gpu
+    '''
+    mod = SourceModule("""
+    __global__ void multiply_them(float *dest, float *a, float *b)
+    {
+    const int i = threadIdx.x;
+    dest[i] = a[i] * b[i];
+    }
+    """)
+
+    multiply_them = mod.get_function("multiply_them")
+
+    a = numpy.random.randn(400).astype(numpy.float32)
+    b = numpy.random.randn(400).astype(numpy.float32)
+
+    dest = numpy.zeros_like(a)
+    multiply_them(
+            drv.Out(dest), drv.In(a), drv.In(b),
+            block=(400, 1, 1))
+
+    numpy.testing.assert_allclose(dest-a*b, 0)
+    print('Used', pycuda.autoinit.device.name())
