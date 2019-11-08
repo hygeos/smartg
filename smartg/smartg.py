hygeos/smartg

commit 2437a7e701503afaab5e33a5159f2085af6c4d80

beginning of the implementation of the obj backward error estimates

smartg/smartg.py
@@ -1381,7 +1381,58 @@ class Smartg(object):
                     cMatVisuRecep[i][:][:] = cMatVisuRecep[i][:][:] * ((surfMir)/(TC*TC*NBPHOTONS))
             else:
                 cMatVisuRecep[:][:][:] = cMatVisuRecep[:][:][:]/(TC*TC*NBPHOTONS)
+
+            print("S2=", TC*TC*nbCx*nbCy)
+            for i in range (0, 8):
+                categories[(i*5)] *= ((surfMir)/(NBPHOTONS))*1000*1000
+                categories[(i*5)+3] *= ((surfMir)/(NBPHOTONS))*1000*1000
+
+            #invMyN = surfMir/((TC*TC*nbCx*nbCy + 0.000485352)*NBPHOTONS)
+            #myN = ((TC*TC*nbCx*nbCy + 0.000485352)*NBPHOTONS)/surfMir
+            # invMyN = surfMir/(TC*TC*nbCx*nbCy*NBPHOTONS)
+            # myN = ((TC*TC*nbCx*nbCy)*NBPHOTONS)/surfMir
+            #myK = 1 + (0.000485352/(TC*TC*nbCx*nbCy))
+            # myK = 1
+            # myK2 = myK * myK
+            # myN = categories[1]+categories[6]+categories[11]+categories[16]+categories[21]+categories[26]+categories[31]+categories[36]
+            # if (myN != 0):
+            #     invMyN = 1./(categories[1]+categories[6]+categories[11]+categories[16]+categories[21]+categories[26]+categories[31]+categories[36])
+            # else:
+            #     invMyN = 0.
             
+            # totW = categories[0]+categories[5]+categories[10]+categories[15]+categories[20]+categories[25]+categories[30]+categories[35]
+            # totW2 = categories[4]+categories[9]+categories[14]+categories[19]+categories[24]+categories[29]+categories[34]+categories[39]
+            # totW = invMyN * totW
+            # totW2 = invMyN * totW2
+            # trueS = np.sqrt(  (myN/(myN-1)) * ( totW2 - (totW*totW) )  )
+            # trueS = trueS/np.sqrt(myN)
+            # trueSP = (trueS/totW)*100
+            # print("trueS=", trueS, "trueSP=", trueSP)
+
+            # for i in range (0, 8):
+            #     # categories[(i*5)] = invMyN * myK * categories[(i*5)] 
+            #     # categories[(i*5)+4] = invMyN * myK2 * categories[(i*5)+4]
+            #     myN = categories[(i*5)+1]/0.90767
+            #     if (myN != 0):
+            #         invMyN = 0.90767/categories[(i*5)+1]
+            #     else:
+            #         invMyN = 0.
+            #     categories[(i*5)] = invMyN * categories[(i*5)]
+            #     categories[(i*5)+4] = invMyN * categories[(i*5)+4]
+        
+            # for i in range (0, 8):
+            #     # Erreur relatives et absolues pour chaque categories
+            #     if (categories[(i*5)] == 0 or categories[(i*5)+4] == 0):
+            #         categories[(i*5)+2] = 0. # relative err
+            #         categories[(i*5)+3] = 0. # abs err
+            #     else:
+            #         #myN = categories[(i*5)+1]
+            #         myS = np.sqrt(  (myN/(myN-1)) * ( categories[(i*5)+4] - (categories[(i*5)]*categories[(i*5)]) )  )
+            #         print("S=", myS, "Zbar=", categories[(i*5)], "Z2bar=", categories[(i*5)+4], "Z2bar-Zbar2=",
+            #               categories[(i*5)+4]-(categories[(i*5)]*categories[(i*5)]))
+            #         categories[(i*5)+3] = myS/np.sqrt(myN)
+            #         categories[(i*5)+2] = (categories[(i*5)+3]/categories[(i*5)])*100
+
         # If there are no heliostats --> there is no STP and then no analyses of optical losses
         if (nb_H <= 0):
             dicSTP = None; vecLoss = None;
@@ -1709,14 +1760,16 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         
         
     if (cats is not None):
-        m.add_dataset('catWeightPh', np.array([cats[0], cats[4], cats[8], cats[12], cats[16], cats[20],
-                                               cats[24], cats[28]], dtype=np.float64), ['Categories'])
-        m.add_dataset('catNbPh', np.array([cats[1], cats[5], cats[9], cats[13], cats[17], cats[21],
-                                           cats[25], cats[29]], dtype=np.float64), ['Categories'])
-        m.add_dataset('catErrP', np.array([cats[2], cats[6], cats[10], cats[14], cats[18], cats[22], 
-                                           cats[26], cats[30]], dtype=np.float64), ['Categories'])
-        m.add_dataset('catErrAbs', np.array([cats[3], cats[7], cats[11], cats[15], cats[19], cats[23], 
-                                             cats[27], cats[31]], dtype=np.float64), ['Categories'])
+        m.add_dataset('catWeightPh', np.array([cats[0], cats[5], cats[10], cats[15], cats[20], cats[25],
+                                               cats[30], cats[35]], dtype=np.float64), ['Categories'])
+        m.add_dataset('catNbPh', np.array([cats[1], cats[6], cats[11], cats[16], cats[21], cats[26],
+                                           cats[31], cats[36]], dtype=np.float64), ['Categories'])
+        m.add_dataset('catErrP', np.array([cats[2], cats[7], cats[12], cats[17], cats[22], cats[27], 
+                                           cats[32], cats[37]], dtype=np.float64), ['Categories'])
+        m.add_dataset('catErrAbs', np.array([cats[3], cats[8], cats[13], cats[18], cats[23], cats[28], 
+                                             cats[33], cats[38]], dtype=np.float64), ['Categories'])
+        m.add_dataset('catWeightPh2', np.array([cats[4], cats[9], cats[14], cats[19], cats[24], cats[29], 
+                                                cats[34], cats[39]], dtype=np.float64), ['Categories'])
 
     if (losses is not None):
         # find the atm layer where the mean heliostats z altitude is located
@@ -2202,30 +2255,34 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NOCE, MAX_HIST, NLOW,
         nbPhCat = gpuzeros(8, dtype=np.uint64) # vector to fill the number of photons for  each categories
         if double:
             wPhCat = gpuzeros(8, dtype=np.float64)  # vector to fill the weight of photons for each categories
+            wPhCat2 = gpuzeros(8, dtype=np.float64)  # sum of squared photons weight for each cats
             tabObjInfo = gpuzeros((9, nbCx, nbCy), dtype=np.float64)
             wPhLoss = gpuzeros(5, dtype=np.float64)
         else:
             wPhCat = gpuzeros(8, dtype=np.float32)
+            wPhCat2 = gpuzeros(8, dtype=np.float32)
             tabObjInfo = gpuzeros((9, nbCx, nbCy), dtype=np.float32)
             wPhLoss = gpuzeros(5, dtype=np.float32)
         tabMatRecep = np.zeros((9, nbCx, nbCy), dtype=np.float64)  
         # vecteur comprenant : weightPhotons, nbPhoton, err% et errAbs pour
         # les 8 categories donc 4 x 8 valeurs = 32. vecCat[0], [1], [2] et [3]
         # pour la categorie 1 et ainsi de suite...
-        vecCats = np.zeros((32), dtype=np.float64)
+        vecCats = np.zeros((40), dtype=np.float64)
         # vector where: v[0]=Wi, v[1]=Wi/(n.dirS), v[2]=Wo, v[3]=Wr 
         vecLoss = np.zeros((5), dtype=np.float64)
     else:
         nbPhCat = gpuzeros(1, dtype=np.uint64)
         if double:
             wPhCat = gpuzeros(1, dtype=np.float64)
+            wPhCat2 = gpuzeros(1, dtype=np.float64)
             wPhLoss = gpuzeros(1, dtype=np.float64)
             tabObjInfo = gpuzeros((1, 1, 1), dtype=np.float64)
         else:
             wPhCat = gpuzeros(1, dtype=np.float32)
+            wPhCat2 = gpuzeros(1, dtype=np.float32)
             wPhLoss = gpuzeros(1, dtype=np.float32)
             tabObjInfo = gpuzeros((1, 1, 1), dtype=np.float32)
-            
+        
     # Initialize the array for error counting
     NERROR = 32
     errorcount = gpuzeros(NERROR, dtype='uint64')
@@ -2284,6 +2341,7 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NOCE, MAX_HIST, NLOW,
         # en rapport avec les objets
         tabObjInfo.fill(0)
         wPhCat.fill(0)
+        wPhCat2.fill(0)
         wPhLoss.fill(0)
         
         start_cuda_clock = cuda.Event()
@@ -2297,7 +2355,7 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NOCE, MAX_HIST, NLOW,
                  errorcount, nThreadsActive, tabPhotons, tabDist, tabHist,
                  Counter, NPhotonsIn, NPhotonsOut, tabthv, tabphi, tab_sensor,
                  prof_atm, prof_oc, wl_proba_icdf, rng.state, tabObjInfo, myObjects0, nbPhCat, wPhCat,
-                 wPhLoss, block=(XBLOCK, 1, 1), grid=(XGRID, 1, 1))
+                 wPhCat2, wPhLoss, block=(XBLOCK, 1, 1), grid=(XGRID, 1, 1))
         else:
             kern(spectrum, X0, faer, foce,
                  errorcount, nThreadsActive, tabPhotons, tabDist, tabHist,
@@ -2318,8 +2376,8 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NOCE, MAX_HIST, NLOW,
             vecLoss += wPhLoss[:].get()
             for i in range (0, 8):
                 # Comptage des poids pour chaque categories
-                vecCats[i*4] += wPhCat[i].get();
-
+                vecCats[i*5] += wPhCat[i].get();
+                vecCats[(i*5)+4] += wPhCat2[i].get();
         
         L = NPhotonsIn   # number of photons launched by last kernel
         NPhotonsInTot += L
@@ -2358,16 +2416,16 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NOCE, MAX_HIST, NLOW,
     if TC is not None:
         for i in range (0, 8):
             # # Comptage des poids pour chaque categories
-            # vecCats[i*4] = wPhCat[i].get();
+            # vecCats[i*5] = wPhCat[i].get();
             # Comptage du nombre de photons pour chaque categories
-            vecCats[(i*4)+1] = nbPhCat[i].get();
+            vecCats[(i*5)+1] = nbPhCat[i].get();
             # Erreur relatives et absolues pour chaque categories
-            if (vecCats[i*4] == 0 or vecCats[(i*4)+1] == 0):
-                vecCats[(i*4)+2] = 0.
-                vecCats[(i*4)+3] = 0.
+            if (vecCats[i*5] == 0 or vecCats[(i*5)+1] == 0):
+                vecCats[(i*5)+2] = 0.
+                vecCats[(i*5)+3] = 0.
             else:    
-                vecCats[(i*4)+2] = float((100.*(1./vecCats[(i*4)+1]**0.5)));
-                vecCats[(i*4)+3] = vecCats[i*4]*(1./vecCats[(i*4)+1]**0.5);
+                vecCats[(i*5)+2] = float((100.*(1./vecCats[(i*5)+1]**0.5)));
+                vecCats[(i*5)+3] = vecCats[i*5]*(1./vecCats[(i*5)+1]**0.5);
     else:
         tabMatRecep = None
         vecCats = None

smartg/src/device.cu
@@ -45,7 +45,7 @@ extern "C" {
 							 , void *tabObjInfo,
 							 struct IObjets *myObjets,
 							 unsigned long long *nbPhCat,
-							 void *wPhCat,
+							 void *wPhCat, void *wPhCat2,
 							 void *wPhLoss
 							 #endif
 							 ) {
@@ -273,7 +273,7 @@ extern "C" {
 
 		#if defined(BACK) && defined(OBJ3D)
 		if (count_level == UPTOA and LMODEd == 4) // the photon reach TOA
-		{ countPhotonObj3D(&ph, tabObjInfo, &geoStruc, nbPhCat, wPhCat);}
+		{ countPhotonObj3D(&ph, tabObjInfo, &geoStruc, nbPhCat, wPhCat, wPhCat2);}
         #endif
 
 		__syncthreads();
@@ -671,7 +671,7 @@ extern "C" {
 		{
 
 			if (geoStruc.type == RECEIVER and LMODEd != 4) // this is a receiver
-			{ countPhotonObj3D(&ph, tabObjInfo, &geoStruc, nbPhCat, wPhCat);}
+			{ countPhotonObj3D(&ph, tabObjInfo, &geoStruc, nbPhCat, wPhCat, wPhCat2);}
 
 			// For losses count
 			ph.weight_loss[0] = ph.weight;
@@ -1455,13 +1455,13 @@ __device__ void initPhoton(Photon* ph, struct Profile *prof_atm, struct Profile 
 		
 			double PHconed, THconed;
 
-			// disk cone sampling
-			// Mixte between disk sampling (Dunn & Shultis 2011) and cone sampling 
-			double rd, Rd;
-			PHconed = 360*RAND;
-			Rd = tan(radiansd(SUN_DISCd));
-			rd = Rd*sqrt(RAND);
-			THconed = atan(rd)*180/PI;
+			// // disk cone sampling
+			// // Mixte between disk sampling (Dunn & Shultis 2011) and cone sampling 
+			// double rd, Rd;
+			// PHconed = 360*RAND;
+			// Rd = tan(radiansd(SUN_DISCd));
+			// rd = Rd*sqrt(RAND);
+			// THconed = atan(rd)*180/PI;
 
 			// Lambertian cone sampling, see Dutré 2003
 			// double sinTHCone, sinTH;
@@ -1469,6 +1469,12 @@ __device__ void initPhoton(Photon* ph, struct Profile *prof_atm, struct Profile 
 			// sinTHCone = sin(radiansd(SUN_DISCd));
 			// sinTH = sqrt(RAND)*sinTHCone;
 			// THconed = asin(sinTH)*180./CUDART_PI;
+
+            // Isotropic cone sampling, see Dutré 2003
+			double cosTHCone;
+			PHconed = 360*RAND;
+			cosTHCone = cos(radiansd(SUN_DISCd));
+			THconed = acos(RAND*(cosTHCone-1)+1)*180./CUDART_PI;
 
 			// Transformations to consider the solar cone sampling	
 			Transformd TPHconed, TTHconed;
@@ -4594,7 +4600,7 @@ __device__ void countLoss(Photon* ph, IGeo* geoS, void *wPhLoss, struct Sensor *
 	#endif
 }
 
-__device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsigned long long *nbPhCat, void *wPhCat)
+__device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsigned long long *nbPhCat, void *wPhCat, void *wPhCat2)
 {
 	int indI = 0; int indJ = 0;
 	float3 p_t; float sizeX = nbCx*TCd; float sizeY = nbCy*TCd;
@@ -4654,24 +4660,27 @@ __device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsig
 	
     #ifdef DOUBLE
 	double *tabCountObj;
-	double *wPhCatC;
+	double *wPhCatC, *wPhCatC2;
 	double4 stokes;
-	double weight;
+	double weight, weight2;
 	tabCountObj = (double*)tabObjInfo;
 	wPhCatC = (double*)wPhCat;
+	wPhCatC2 = (double*)wPhCat2;
 	stokes = make_double4(ph->stokes.x, ph->stokes.y, ph->stokes.z, ph->stokes.w);
 	weight = (double)ph->weight;
 	weight = weight * double(stokes.x + stokes.y);
     #else // If not DOUBLE
 	float *tabCountObj;
-	float *wPhCatC;
-	float weight;
+	float *wPhCatC, *wPhCatC;
+	float weight, weight2;
 	tabCountObj = (float*)tabObjInfo;
 	wPhCatC = (float*)wPhCat;
+	wPhCatC2 = (float*)wPhCat2;
 	weight = (float)ph->weight;
 	weight = weight * float(ph->stokes.x + ph->stokes.y);
 	#endif
-	
+	weight2 = weight * weight;
+
 	if(isnan(weight))
 	{
 		printf("Care weight is nan !! \n");
@@ -4686,37 +4695,37 @@ __device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsig
 	// Les huit catégories
 	if (ph->H == 0 && ph->E == 0 && ph->S == 0) 
 	{ // CAT 1 : aucun changement de trajectoire avant de toucher le R.
-	    atomicAdd(wPhCatC, weight); // comptage poids
+	    atomicAdd(wPhCatC, weight); atomicAdd(wPhCatC2, weight2);// comptage poids
 		atomicAdd(nbPhCat, 1);     // comptage nombre de photons
 		atomicAdd(tabCountObj+(nbCy*nbCx)+(nbCy*indI)+indJ, weight); // distri
 	}
 	else if ( ph->H > 0 && ph->E == 0 && ph->S == 0)
 	{ // CAT 2 : only H avant de toucher le R.
-		atomicAdd(wPhCatC+1, weight);
+		atomicAdd(wPhCatC+1, weight); atomicAdd(wPhCatC2+1, weight2);
 		atomicAdd(nbPhCat+1, 1);
 		atomicAdd(tabCountObj+(2*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H == 0 && ph->E > 0 && ph->S == 0)
 	{ // CAT 3 : only E avant de toucher le R.
-		atomicAdd(wPhCatC+2, weight);
+		atomicAdd(wPhCatC+2, weight); atomicAdd(wPhCatC2+2, weight2);
 		atomicAdd(nbPhCat+2, 1);
 		atomicAdd(tabCountObj+(3*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H == 0 && ph->E == 0 && ph->S > 0)
 	{ // CAT 4 : only S avant de toucher le R.
-		atomicAdd(wPhCatC+3, weight);
+		atomicAdd(wPhCatC+3, weight); atomicAdd(wPhCatC2+3, weight2);
 		atomicAdd(nbPhCat+3, 1);
 		atomicAdd(tabCountObj+(4*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H > 0 && ph->E == 0 && ph->S > 0)
 	{ // CAT 5 : 2 proc. H et S avant de toucher le R.
-		atomicAdd(wPhCatC+4, weight);
+		atomicAdd(wPhCatC+4, weight); atomicAdd(wPhCatC2+4, weight2);
 		atomicAdd(nbPhCat+4, 1);
 		atomicAdd(tabCountObj+(5*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H > 0 && ph->E > 0 && ph->S == 0)
 	{ // CAT 6 : 2 proc. H et E avant de toucher le R.
-		atomicAdd(wPhCatC+5, weight);
+		atomicAdd(wPhCatC+5, weight); atomicAdd(wPhCatC2+5, weight2);
 		atomicAdd(nbPhCat+5, 1);
 		atomicAdd(tabCountObj+(6*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
         //printf("H = %d, E = %d, S = %d", ph->H, ph->E, ph->S);
@@ -4724,13 +4733,13 @@ __device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsig
 	}
 	else if ( ph->H == 0 && ph->E > 0 && ph->S > 0)
 	{ // CAT 7 : 2 proc. E et S avant de toucher le R.
-		atomicAdd(wPhCatC+6, weight);
+		atomicAdd(wPhCatC+6, weight); atomicAdd(wPhCatC2+6, weight2);
 		atomicAdd(nbPhCat+6, 1);
 		atomicAdd(tabCountObj+(7*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}	
 	else if ( ph->H > 0 && ph->E > 0 && ph->S > 0)
 	{ // CAT 8 : 3 proc. H, E et S avant de toucher le R.
-		atomicAdd(wPhCatC+7, weight);
+		atomicAdd(wPhCatC+7, weight); atomicAdd(wPhCatC2+7, weight2);
 		atomicAdd(nbPhCat+7, 1);
 		atomicAdd(tabCountObj+(8*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}		
@@ -4740,51 +4749,51 @@ __device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsig
 	// Les huit catégories
 	if (ph->H == 0 && ph->E == 0 && ph->S == 0) 
 	{ // CAT 1 : aucun changement de trajectoire avant de toucher le R.
-	    DatomicAdd(wPhCatC, weight); // comptage poids
+	    DatomicAdd(wPhCatC, weight); DatomicAdd(wPhCatC2, weight2);// comptage poids
 		atomicAdd(nbPhCat, 1);     // comptage nombre de photons
 		DatomicAdd(tabCountObj+(nbCy*nbCx)+(nbCy*indI)+indJ, weight); // distri
 	}
 	else if ( ph->H > 0 && ph->E == 0 && ph->S == 0)
 	{ // CAT 2 : only H avant de toucher le R.
-		DatomicAdd(wPhCatC+1, weight);
+		DatomicAdd(wPhCatC+1, weight); DatomicAdd(wPhCatC2+1, weight2);
 		atomicAdd(nbPhCat+1, 1);
 		DatomicAdd(tabCountObj+(2*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 		//printf("H = %d, E = %d, S = %d", ph->H, ph->E, ph->S);
 	}
 	else if ( ph->H == 0 && ph->E > 0 && ph->S == 0)
 	{ // CAT 3 : only E avant de toucher le R.
-		DatomicAdd(wPhCatC+2, weight);
+		DatomicAdd(wPhCatC+2, weight); DatomicAdd(wPhCatC2+2, weight2);
 		atomicAdd(nbPhCat+2, 1);
 		DatomicAdd(tabCountObj+(3*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H == 0 && ph->E == 0 && ph->S > 0)
 	{ // CAT 4 : only S avant de toucher le R.
-		DatomicAdd(wPhCatC+3, weight);
+		DatomicAdd(wPhCatC+3, weight); DatomicAdd(wPhCatC2+3, weight2);
 		atomicAdd(nbPhCat+3, 1);
 		DatomicAdd(tabCountObj+(4*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H > 0 && ph->E == 0 && ph->S > 0)
 	{ // CAT 5 : 2 proc. H et S avant de toucher le R.
-		DatomicAdd(wPhCatC+4, weight);
+		DatomicAdd(wPhCatC+4, weight); DatomicAdd(wPhCatC2+4, weight2);
 		atomicAdd(nbPhCat+4, 1);
 		DatomicAdd(tabCountObj+(5*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
 	else if ( ph->H > 0 && ph->E > 0 && ph->S == 0)
 	{ // CAT 6 : 2 proc. H et E avant de toucher le R.
-		DatomicAdd(wPhCatC+5, weight);
+		DatomicAdd(wPhCatC+5, weight); DatomicAdd(wPhCatC2+5, weight2);
 		atomicAdd(nbPhCat+5, 1);
 		DatomicAdd(tabCountObj+(6*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
         //printf("H = %d, E = %d, S = %d", ph->H, ph->E, ph->S);
 	}
 	else if ( ph->H == 0 && ph->E > 0 && ph->S > 0)
 	{ // CAT 7 : 2 proc. E et S avant de toucher le R.
-		DatomicAdd(wPhCatC+6, weight);
+		DatomicAdd(wPhCatC+6, weight); DatomicAdd(wPhCatC2+6, weight2);
 		atomicAdd(nbPhCat+6, 1);
 		DatomicAdd(tabCountObj+(7*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}	
 	else if ( ph->H > 0 && ph->E > 0 && ph->S > 0)
 	{ // CAT 8 : 3 proc. H, E et S avant de toucher le R.
-		DatomicAdd(wPhCatC+7, weight);
+		DatomicAdd(wPhCatC+7, weight); DatomicAdd(wPhCatC2+7, weight2);
 		atomicAdd(nbPhCat+7, 1);
 		DatomicAdd(tabCountObj+(8*nbCy*nbCx)+(nbCy*indI)+indJ, weight);
 	}
@@ -5836,7 +5845,7 @@ __device__ bool geoTest(float3 o, float3 dir, int phLocPrev, float3* phit, IGeo 
 		// si une valeur en x, y ou z diff de 0 alors il y a une translation
 		if ( (ObjT[i].mvTx>VALMIN and ObjT[i].mvTx<-VALMIN) or
 			 (ObjT[i].mvTy>VALMIN and ObjT[i].mvTy>-VALMIN) or
-			 (ObjT[i].mvTz>VALMIN and ObjT[i].mvTy>-VALMIN)) {
+			 (ObjT[i].mvTz>VALMIN and ObjT[i].mvTz>-VALMIN)) {
 			Transform TmT;
 			TmT = Ti.Translate(make_float3(ObjT[i].mvTx, ObjT[i].mvTy,
 										   ObjT[i].mvTz));

smartg/src/device.h
@@ -116,7 +116,7 @@ __global__ void launchKernel(
 		, void *tabObjInfo,
 		struct IObjets *myObjets,
 		unsigned long long *nbPhCat,
-		void *wPhCat,
+		void *wPhCat, void *wPhCat2,
 		void *wPhLoss
 		#endif
         );
@@ -195,7 +195,7 @@ __device__ void Obj3DRoughSurf(Photon* ph, IGeo* geoS, struct RNG_State *rngstat
 
 __device__ void countLoss(Photon* ph, IGeo* geoS, void *wPhLoss, struct Sensor *tab_sensor);
 
-__device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsigned long long *nbPhCat, void *wPhCat);
+__device__ void countPhotonObj3D(Photon* ph, void *tabObjInfo, IGeo* geoS, unsigned long long *nbPhCat, void *wPhCat, void *wPhCat2);
 #endif
 
 __device__ void countPhoton(Photon* , struct Profile* prof_atm, struct Profile* prof_oc, float*, float *,

smartg/visualizegeo.py
@@ -89,7 +89,7 @@ def receiver_view(disMatrix, w = False, logI=False, nameFile = None, MTOA = 1320
         plt.savefig(nameFile + '.pdf')  
 
 
-def cat_view(mlut, view = 'all', acc = 6):
+def cat_view(mlut, view = 'all', acc = 6, MTOA = 1320):
     '''
     Definition of cat_view
 
@@ -103,19 +103,23 @@ def cat_view(mlut, view = 'all', acc = 6):
     strAcc = str(intAcc)
     # strAcc = "{0:." + strAcc + "}"
     strAcc = "%." + strAcc + "f"
-    
+
+    print("**************************************************")
+    print("Intensity in power unit (Watt) for each categories")
+    print("Irradiance(W/m²)=Intensity(W)/receiverSurface(m²)")
+    print("**************************************************")
     for i in range (0, 8):
         if (view == 'all'):
-            print("CAT",i+1, lP[i], ": weight=", strAcc % mlut['catWeightPh'][i], " number=", np.uint64(mlut['catNbPh'][i]),
-                  " err(%)=", strAcc % mlut['catErrP'][i], " errAbs=", strAcc % mlut['catErrAbs'][i])
+            print("CAT",i+1, lP[i], ": weight=", strAcc % (mlut['catWeightPh'][i]*MTOA), " number=", np.uint64(mlut['catNbPh'][i]),
+                  " err(%)=", strAcc % mlut['catErrP'][i], " errAbs=", strAcc % (mlut['catErrAbs'][i]*MTOA))
         elif (view == 'weight'):
-            print("CAT",i+1,": weight=", strAcc % mlut['catWeightPh'][i])
+            print("CAT",i+1,": weight=", strAcc % (mlut['catWeightPh'][i]*MTOA))
         elif (view == 'number'):
             print("CAT",i+1,": number=", np.uint64(mlut['catNbPh'][i]))
         elif (view == 'errP'):
             print("CAT",i+1,": err(%)=", strAcc % mlut['catErrP'][i])
         elif (view == 'errAbs'):
-            print("CAT",i+1,": errAbs=", strAcc % mlut['catErrAbs'][i])
+            print("CAT",i+1,": errAbs=", strAcc % (mlut['catErrAbs'][i]*MTOA))
 
 
 class Mirror(object):

commit 3d8371c3b23b345f8ff481903c3adf0993ab4c2a

Correction of a bug on the attenuation loss introduced by the commit: 2437a7e701503afa

smartg/smartg.py
@@ -1368,10 +1368,12 @@ class Smartg(object):
         attrs.update(self.common_attrs)
 
         # En rapport avec l'implémentation des objets (permet le visuel des res du recept)
+        weightR = 0 # for the attenuation loss
         if (TC is not None):
             if (cusL is None):
                 cMatVisuRecep[:][:][:] = cMatVisuRecep[:][:][:]
             elif (cusL.dict['LMODE'] != "B" and cusL.dict['LMODE'] != "BR"):
+                weightR = categories[5] # for the att loss, weight of cat2 absorbed by the receiver
                 for i in range (0, 9):
                     cMatVisuRecep[i][:][:] = cMatVisuRecep[i][:][:] * ((surfMir)/(TC*TC*NBPHOTONS))
                 for i in range (0, 8):
@@ -1386,7 +1388,7 @@ class Smartg(object):
         else:
             MZAlt_H = zAlt_H/nb_H
             # dicSTP : tuple incorporating parameters for Solar Tower Power applications
-            dicSTP = {"nb_H":nb_H, "totS_H":totS_H, "surfTOA":surfMir, "MZAlt_H":MZAlt_H, "vSun":vSun}
+            dicSTP = {"nb_H":nb_H, "totS_H":totS_H, "surfTOA":surfMir, "MZAlt_H":MZAlt_H, "vSun":vSun, "wRec":weightR}
                 
         # finalization
         output = finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl[:], NPhotonsInTot, errorcount, NPhotonsOutTot,
@@ -1747,7 +1749,7 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         nref = max(0, min(losses[2]/losses[0], 1))
         nspi = max(0, min(losses[3]/losses[2], 1))
         nblo = max(0, min((losses[3]-losses[4])/losses[3], 1))
-        natm = max(0, min(cats[4]/(losses[3]-losses[4]), 1))
+        natm = max(0, min(dicSTP["wRec"]/(losses[3]-losses[4]), 1))
         m.add_dataset('n_sha', np.array([nsha], dtype=np.float64), ['Shadow Efficiency'])
         m.add_dataset('n_cos', np.array([ncos], dtype=np.float64), ['Cosine Efficiency'])
         m.add_dataset('n_ref', np.array([nref], dtype=np.float64), ['Reflection Efficiency'])