hygeos/smartg

commit f20674c6ff1d2c0cb241422e58f9cd4f879e85a4

Modification of the isotropic sampling in FF mode and begining of some changes linked with opt losses

smartg/smartg.py
@@ -787,7 +787,10 @@ class Smartg(object):
             elif (cusL.dict['LMODE'] == "FF"):
                 # The projected surface at TOA where the photons are launched
                 DotNN = Dot(vSun*-1, Vector(0., 0., 1.))
-                surfLPH = float(cusL.dict['CFX'])*float(cusL.dict['CFY'])*DotNN
+                if (cusL.dict['TYPE'] == 2 and cusL.dict['FOV'] > 1e-6): #isotropic
+                    surfLPH = float(cusL.dict['CFX'])*float(cusL.dict['CFY'])
+                else:
+                    surfLPH = float(cusL.dict['CFX'])*float(cusL.dict['CFY'])*DotNN
 
         #
         # initialization
@@ -805,7 +808,7 @@ class Smartg(object):
 
             # Initiliaze all the parameters linked with 3D objects
             (nGObj, nObj, nRObj, surfLPH_RF, nb_H, zAlt_H, totS_H, TC, nbCx, nbCy,
-             myObjects0, myGObj0, myRObj0) = initObj(LGOBJ=myObjects, vSun=vSun, CUSL=cusL)
+             myObjects0, myGObj0, myRObj0, n_cos) = initObj(LGOBJ=myObjects, vSun=vSun, CUSL=cusL)
 
             # If we are in RF mode don't forget to update the value of surfLPH
             if (surfLPH_RF is not None): surfLPH = surfLPH_RF
@@ -1095,7 +1098,7 @@ class Smartg(object):
         if (nb_H > 0 and TC is not None and cusL is not None):
             MZAlt_H = zAlt_H/nb_H; weightR=matCats[2, 1]; SREC=TC*TC*nbCx*nbCy;
             # dicSTP : tuple incorporating parameters for Solar Tower Power applications
-            dicSTP = {"nb_H":nb_H, "totS_H":totS_H, "surfTOA":surfLPH, "MZAlt_H":MZAlt_H, "vSun":vSun, "wRec":matCats[2, 1],
+            dicSTP = {"nb_H":nb_H, "n_cos": n_cos, "totS_H":totS_H, "surfTOA":surfLPH, "MZAlt_H":MZAlt_H, "vSun":vSun, "wRec":matCats[2, 1],
                       "SREC":SREC, "TC":TC, "LPH":cusL.dict['LPH'], "LPR":cusL.dict['LPR'], "prog":progress, "n_cte":n_cte}
         # If there are no heliostats --> no analyses of optical losses
         elif(TC is not None and cusL is not None):
@@ -1459,7 +1462,8 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         m.add_dataset('wLoss', np.array(matLoss[:,0], dtype=np.float64), ['index'])
         m.add_dataset('wLoss2', np.array(matLoss[:,1], dtype=np.float64), ['index'])
         m.set_attr('n_cte', str(dicSTP["n_cte"]))
-
+        m.set_attr('n_cos', str(dicSTP["n_cos"]))
+        
         # To consider also the multispectral case
         if (NLAM > 1) : lwl = len(wl)
         else : lwl = 1
@@ -2428,7 +2432,7 @@ def initObj(LGOBJ, vSun, CUSL=None):
 
     # Initialization before the coming loop
     pp1 = 0.; pp2 = 0.; pp3 = 0.; pp4 = 0.;
-    nb_H = 0; zAlt_H = 0.; totS_H = 0.;
+    nb_H = 0; zAlt_H = 0.; totS_H = 0.; ncos=0.;
     if (CUSL != None and CUSL.dict['LMODE'] == "RF"): surfLPH = 0;
     else: surfLPH = None;
 
@@ -2576,6 +2580,7 @@ def initObj(LGOBJ, vSun, CUSL=None):
                 nb_H += 1
                 zAlt_H += LOBJ[i].transformation.transz
                 totS_H += abs(LOBJ[i].geo.p1.x)*abs(LOBJ[i].geo.p1.y)*4;
+                ncos += Dot(normalBase, Vector(-vSun.x, -vSun.y, -vSun.z))
 
             # Crucial step for the result visualization in RF mode
             if (CUSL is not None and CUSL.dict['LMODE'] == "RF"):
@@ -2627,8 +2632,12 @@ def initObj(LGOBJ, vSun, CUSL=None):
 
     LOBJGPU = to_gpu(LOBJGPU)
     LROBJGPU = to_gpu(LROBJGPU)
+    # update the value of ncos
+    n_cos = ncos/nb_H
+    #print("ncos=", ncos/nb_H, "nbH=", nb_H)
 
-    return nGObj, nObj, nRObj, surfLPH, nb_H, zAlt_H, totS_H, TC, nbCx, nbCy, LOBJGPU, LGOBJGPU, LROBJGPU
+    return nGObj, nObj, nRObj, surfLPH, nb_H, zAlt_H, totS_H, TC, nbCx, nbCy, LOBJGPU, \
+        LGOBJGPU, LROBJGPU, n_cos
 
 def normalizeRecIrr(cMatVisuRecep, matCats, nbCx, nbCy, NBPHOTONS, surfLPH, TC, cusL, SUN_DISC):
     '''
@@ -2673,10 +2682,10 @@ def normalizeRecIrr(cMatVisuRecep, matCats, nbCx, nbCy, NBPHOTONS, surfLPH, TC, 
         normC = (surfLPH*1e6)/NBPHOTONS  # Here multiply by 1e6 to convert km² to m²
         normFF = 1.
         #lambertian sampling normalization
-        if (cusL.dict['LMODE'] == "FF" and cusL.dict['TYPE'] == 1 and cusL.dict['FOV'] > 0):
+        if (cusL.dict['LMODE'] == "FF" and cusL.dict['TYPE'] == 1 and cusL.dict['FOV'] > 1e-6):
             normFF = ( 1-np.cos(np.radians(2*cusL.dict['FOV'])) ) / (4*( 1-np.cos(np.radians(cusL.dict['FOV'])) ))
         #isotropic sampling normalization
-        elif (cusL.dict['LMODE'] == "FF" and cusL.dict['TYPE'] == 2):
+        elif (cusL.dict['LMODE'] == "FF" and cusL.dict['TYPE'] == 2 and cusL.dict['FOV'] > 1e-6):
             normFF = 1.
         normC *= normFF
         for i in range (0, 9):

smartg/src/device.cu
@@ -18,7 +18,6 @@
 #include "transform.h"
 
 #include <math.h>
-
 #include <math_constants.h>
 #include <helper_math.h>
 #include <stdio.h>
@@ -1382,7 +1381,7 @@ __device__ void initPhoton(Photon* ph, struct Profile *prof_atm, struct Profile 
 		ph->pos.y = PYd + cusForwPos.y + CFTYd;
 		ph->pos.z = PZd;
 
-		if (ALDEGd != 0)
+		if (ALDEGd > 1e-6)
 		{
 			// Initialization of the cosine vector (v), the orthogonal vector (u), ...
 			float3 vfloat = make_float3(0., 0., -1.); float3 ufloat = make_float3(-1., 0., 0.);
@@ -1404,7 +1403,7 @@ __device__ void initPhoton(Photon* ph, struct Profile *prof_atm, struct Profile 
 				PHcone = 360*RAND;
 				cosTHCone = __cosf(radians(ALDEGd));
 				THcone = acosf(RAND*(cosTHCone-1)+1)*180./CUDART_PI_F;
-				ph->weight *= __cosf(radians(THcone));
+				//ph->weight *= __cosf(radians(THcone));
 			}
 			if (TYPEd == 3) //in development
 			{
@@ -1430,11 +1429,16 @@ __device__ void initPhoton(Photon* ph, struct Profile *prof_atm, struct Profile 
 			TTheta = TTheta.RotateY(sunTheta); TPhi = TPhi.RotateZ(sunPhi);
 			vfloat = TPhi(   Vectorf(  TTheta( Vectorf(vfloat) )  )   );
 			ufloat = TPhi(   Vectorf(  TTheta( Vectorf(ufloat) )  )   );
-
+			
 			// Update the values of u and v
 			ph->v = make_float3(vfloat.x, vfloat.y, vfloat.z);
 			ph->u = make_float3(ufloat.x, ufloat.y, ufloat.z);
-		} // END ALDEGd != 0
+
+			if (TYPEd == 2)
+			{
+				ph->weight *= dot(ph->v, make_float3(0., 0., -1.));
+			}
+		} // END ALDEGd > 0
 
 	} //END LMODEd == 2
     #else // else if Backward modes

smartg/visualizegeo.py
@@ -192,10 +192,14 @@ def nopt_view(MLUT, BACK=False, ACC = 6, NCL="68%", fl_TOA=None, NAATM=False):
         for i in range (0, len(sum2Z)):
             dw_temp = ld*NBIS*(sumZ2[i]-sum2Z[i])**0.5
             dw.append(dw_temp)
-        nopt = Clamp(k*w5, 0, 1)
+            
+        k_s = k/float(m.attrs['n_cos']);
+        nsha_n = Clamp(k_s*w0, 0, 1); ncos_n = float(m.attrs['n_cos']);
+        nopt = Clamp(k*w5, 0, 1);
         nsha = Clamp(k*w1, 0, 1); ncos = Clamp(w0/w1, 0, 1); nref = Clamp(w2/w0, 0, 1);
         nspi = Clamp(w3/w2, 0, 1); nblo = Clamp(1-(w4/w3), 0, 1); natm = Clamp(w5/(w3-w4), 0, 1);
-        
+
+        d_nsha_n = abs(k_s)*dw[0]
         d_nopt = abs(k)*dw[5]; d_nsha = abs(k)*dw[1]
         d_ncos = abs(1./w1)*dw[0] + abs(-w0/w1**2)*dw[1]
         d_nref = abs(1./w0)*dw[2] + abs(-w2/w0**2)*dw[0]
@@ -205,7 +209,9 @@ def nopt_view(MLUT, BACK=False, ACC = 6, NCL="68%", fl_TOA=None, NAATM=False):
 
         print("nopt =", strAcc % nopt, ", errAbs =", strAcc % d_nopt, ", err% =", strAcc % ((d_nopt/nopt)*100))
         print("nsha =", strAcc % nsha, ", errAbs =", strAcc % d_nsha, ", err% =", strAcc % ((d_nsha/nsha)*100))
+        print("nsha_n =", strAcc % nsha_n, ", errAbs =", strAcc % d_nsha_n, ", err% =", strAcc % ((d_nsha_n/nsha_n)*100))
         print("ncos =", strAcc % ncos, ", errAbs =", strAcc % d_ncos, ", err% =", strAcc % ((d_ncos/ncos)*100))
+        print("ncos_n =", strAcc % ncos_n, ", errAbs =", strAcc % 0, ", err% =", strAcc % 0)
         print("nref =", strAcc % nref, ", errAbs =", strAcc % d_nref, ", err% =", strAcc % ((d_nref/nref)*100))
         print("nspi =", strAcc % nspi, ", errAbs =", strAcc % d_nspi, ", err% =", strAcc % ((d_nspi/nspi)*100))
         print("nblo =", strAcc % nblo, ", errAbs =", strAcc % d_nblo, ", err% =", strAcc % ((d_nblo/nblo)*100))

commit 7feef69c7e6a1172a846bc707498ddecfa9e6bea

Correction of the bug introduced by the commit f20674c6ff1d2c

smartg/smartg.py
@@ -2637,8 +2637,10 @@ def initObj(LGOBJ, vSun, CUSL=None):
     LOBJGPU = to_gpu(LOBJGPU)
     LROBJGPU = to_gpu(LROBJGPU)
     # update the value of ncos
-    n_cos = ncos/nb_H
-    #print("ncos=", ncos/nb_H, "nbH=", nb_H)
+    if nb_H > 0:
+        n_cos = ncos/nb_H
+    else:
+        n_cos = 1
 
     return nGObj, nObj, nRObj, surfLPH, nb_H, zAlt_H, totS_H, TC, nbCx, nbCy, LOBJGPU, \
         LGOBJGPU, LROBJGPU, n_cos