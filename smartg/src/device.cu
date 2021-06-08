hygeos/smartg

commit a6f6846fe922cc274054f2ce9480bf924f7f5f91

Kernel 2 passes validation with ARTDECO

smartg/src/device.cu
@@ -5604,10 +5604,10 @@ __device__ void countPhoton(Photon* ph,
           //int idx = blockIdx.x * blockDim.x + threadIdx.x;
           unsigned long long counter2;
           counter2=atomicAdd(NPhotonsOut, 1);
-          //counter2=atomicAdd(NPhotonsOut+is, 1);
           if (counter2 >= MAX_HIST) return;
-          unsigned long long KK2 = K*(NATM_ABSd+NOCE_ABSd+4+NLOWd+5); /* Number of information per local estmate photon (Record length)*/
-          //unsigned long long KK2 = K*(NATMd+NOCEd+4+NLOWd+3); /* Number of information per local estmate photon (Record length)*/
+          unsigned long long KK2 = NATM_ABSd+NOCE_ABSd+4+NLOWd+6; /* Number of information per local estmate photon (Record length)*/
+          //unsigned long long KK2 = K*(NATM_ABSd+NOCE_ABSd+4+NLOWd+5); /* Number of information per local estmate photon (Record length)*/
+          //unsigned long long KK2 = K*(NATMd+NOCEd+4+NLOWd+5); /* Number of information per local estmate photon (Record length)*/
           //unsigned long long KKK2= KK2 * MAX_HIST; /* Number of individual information per vertical Level (Number of Records)*/
           unsigned long long LL2;
           tabCount3   = (float*)tabHist     ; /* we position the pointer at the good vertical level*/
@@ -5615,37 +5615,42 @@ __device__ void countPhoton(Photon* ph,
           for (int n=0; n<NOCE_ABSd; n++){
                 /* The offset is the number of previous writing (counter2) * Record Length
                    + the offset of the individual information  + the place of the physical quantity */
-                LL2 = counter2*KK2 +  n*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+                LL2 = counter2*KK2 +  n;
+                //LL2 = counter2*KK2 +  n*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
                 tabCount3[LL2]= ph->cdist_oc[n+1];
           }
           //for (int n=0; n<NATMd; n++){
           for (int n=0; n<NATM_ABSd; n++){
-                LL2 = counter2*KK2 +  (n+NOCE_ABSd)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+                LL2 = counter2*KK2 +  n+NOCE_ABSd;
+                //LL2 = counter2*KK2 +  (n+NOCE_ABSd)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
                 tabCount3[LL2]= ph->cdist_atm[n+1];
           }
-          LL2 = counter2*KK2 +  (NATM_ABSd+NOCE_ABSd+0)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NATM_ABSd+NOCE_ABSd+0;
+          //LL2 = counter2*KK2 +  (NATM_ABSd+NOCE_ABSd+0)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
           tabCount3[LL2]= weight * (st.x+st.y);
-          LL2 = counter2*KK2 +  (NATM_ABSd+NOCE_ABSd+1)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NATM_ABSd+NOCE_ABSd+1;
           tabCount3[LL2]= weight * (st.x-st.y);
-          LL2 = counter2*KK2 +  (NATM_ABSd+NOCE_ABSd+2)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NATM_ABSd+NOCE_ABSd+2;
           tabCount3[LL2]= weight * (st.z);
-          LL2 = counter2*KK2 +  (NATM_ABSd+NOCE_ABSd+3)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NATM_ABSd+NOCE_ABSd+3;
           tabCount3[LL2]= weight * (st.w);
 
           for (int n=0; n<NLOWd; n++){
-                LL2 = counter2*KK2 +  (n+NATM_ABSd+NOCE_ABSd+4)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+                LL2 = counter2*KK2 + n+NATM_ABSd+NOCE_ABSd+4;
                 tabCount3[LL2]= weight_sca[n];
           }
-          LL2 = counter2*KK2 +  (NLOWd+NATM_ABSd+NOCE_ABSd+4)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4;
           tabCount3[LL2]= (float)(ph->nrrs>=1);
-          LL2 = counter2*KK2 +  (NLOWd+NATM_ABSd+NOCE_ABSd+4+1)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4+1;
           tabCount3[LL2]= (float)(ph->nref);
-          LL2 = counter2*KK2 +  (NLOWd+NATM_ABSd+NOCE_ABSd+4+2)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4+2;
           tabCount3[LL2]= (float)(ph->nsif);
-          LL2 = counter2*KK2 +  (NLOWd+NATM_ABSd+NOCE_ABSd+4+3)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4+3;
           tabCount3[LL2]= (float)(ph->nvrs>=1);
-          LL2 = counter2*KK2 +  (NLOWd+NATM_ABSd+NOCE_ABSd+4+4)*K + is*NBPHId*NBTHETAd + ith*NBPHId + iphi;
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4+4;
           tabCount3[LL2]= (float)(ph->nenv);
+          LL2 = counter2*KK2 +  NLOWd+NATM_ABSd+NOCE_ABSd+4+5;
+          tabCount3[LL2]= (float)(ph->ith);
        } // HISTd==1
 
        unsigned long long KK  = K*(NATM_ABSd+NOCE_ABSd);
@@ -6859,9 +6864,11 @@ return intensity;
 
 extern "C" {
 __global__ void reduce_absorption_gpu(unsigned long long NPHOTON, unsigned long long NLAYER, unsigned long long NWVL, 
-        unsigned long long NTHREAD, unsigned long long NGROUP, unsigned long long NBUNCH, unsigned long long NP_REST, unsigned long long NWVL_LOW,
+        unsigned long long NTHREAD, unsigned long long NGROUP, unsigned long long NBUNCH, 
+        unsigned long long NP_REST, unsigned long long NWVL_LOW, unsigned long long NBTHETA,
         double *res, double *res_sca, double *res_rrs, double *res_sif, double *res_vrs, float *ab, float *al, float *cd, float *S, float *weight, 
-        unsigned char *nrrs, unsigned char *nref, unsigned char *nsif, unsigned char *nvrs, unsigned char *nenv, unsigned char *iw_low, float *ww_low)
+        unsigned char *nrrs, unsigned char *nref, unsigned char *nsif, unsigned char *nvrs, unsigned char *nenv, unsigned char *ith, 
+        unsigned char *iw_low, float *ww_low)
 {
   const unsigned long long idx = threadIdx.x + blockDim.x * blockIdx.x;
   unsigned long long n,nstart,nstop,ns;
@@ -6870,6 +6877,7 @@ __global__ void reduce_absorption_gpu(unsigned long long NPHOTON, unsigned long 
   double wabs, walb0, walb1,walb; // absorption and albedo high resolution weigths
   float wsca1,wsca2,wsca;
   unsigned long long iw1,iw2;
+  unsigned long long offset;
 
   if (idx<NTHREAD) {
     iw = idx%NWVL ; // index of current wavelength
@@ -6898,11 +6906,13 @@ __global__ void reduce_absorption_gpu(unsigned long long NPHOTON, unsigned long 
         walb *= pow(walb1, (double)nenv[n]);
         for (s=0;s<4;s++) {
             ns = s + 4*n;
-            if (!nsif[n])             atomicAdd(res    +iw+NWVL*s, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
-            if (!nsif[n])             atomicAdd(res_sca+iw+NWVL*s, (double)S[ns] *              (double)wsca * walb);
-            if ( nrrs[n] && !nsif[n]) atomicAdd(res_rrs+iw+NWVL*s, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
-            if ( nsif[n])             atomicAdd(res_sif+iw+NWVL*s, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
-            if ( nvrs[n] && !nsif[n]) atomicAdd(res_vrs+iw+NWVL*s, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            //offset = iw + NWVL*s + NWVL*NBTHETA*ith[n]; 
+            offset = ith[n] + NBTHETA*iw + NWVL*NBTHETA*s; 
+            if (!nsif[n])             atomicAdd(res    +offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if (!nsif[n])             atomicAdd(res_sca+offset, (double)S[ns] *              (double)wsca * walb);
+            if ( nrrs[n] && !nsif[n]) atomicAdd(res_rrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if ( nsif[n])             atomicAdd(res_sif+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if ( nvrs[n] && !nsif[n]) atomicAdd(res_vrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
         }
     }
   }

smartg/smartg.py
@@ -1321,7 +1321,8 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         m.add_dataset('V_stdev_up (TOA)', sigma[UPTOA,3,isen,ilam,iphi,:], axnames)
     m.add_dataset('N_up (TOA)', NPhotonsOutTot[UPTOA,isen,ilam,iphi,:], axnames)
     if len(tabDistFinal) > 1: m.add_dataset('cdist_up (TOA)', tabDistFinal[UPTOA,:,isen,iphi,:], axnames2)
-    if hist : m.add_dataset('disth_up (TOA)', tabHistFinal[:,:,isen,iphi,:],axnames3)
+    #if hist : m.add_dataset('disth_up (TOA)', tabHistFinal[:,:,isen,iphi,:],axnames3)
+    #if hist : m.add_dataset('disth_up (TOA)', np.squeeze(tabHistFinal[:,:,isen,iphi,:]))
 
     if OUTPUT_LAYERS & 1:
         m.add_dataset('I_down (0+)', tabFinal[DOWN0P,0,isen,ilam,iphi,:], axnames)
@@ -1960,7 +1961,7 @@ def reduce_diff(m, varnames, delta=None):
     return res
 
 
-def reduce_histories(kernel2, tabHist, wl, sigma, NLOW, alb_in=None, XBLOCK=512, XGRID=512, verbose=False):
+def reduce_histories(kernel2, tabHist, wl, sigma, NLOW, NBTHETA=1, alb_in=None, XBLOCK=512, XGRID=512, verbose=False):
    NL    = sigma.shape[1]
    w     = tabHist[:, NL+4:-5]
    ngood = np.sum(w[:,0]!=0)
@@ -1968,12 +1969,13 @@ def reduce_histories(kernel2, tabHist, wl, sigma, NLOW, alb_in=None, XBLOCK=512,
    S       = np.zeros((ngood,4),dtype=np.float32) 
    cd      = tabHist[:ngood,     :NL  ]
    S[:,:4] = tabHist[:ngood, NL  :NL+4]
-   w       = tabHist[:ngood, NL+4:-5  ]
-   nrrs    = tabHist[:ngood,      -5  ]
-   nref    = tabHist[:ngood,      -4  ]
-   nsif    = tabHist[:ngood,      -3  ]
-   nvrs    = tabHist[:ngood,      -2  ]
-   nenv    = tabHist[:ngood,      -1  ]
+   w       = tabHist[:ngood, NL+4:-6  ]
+   nrrs    = tabHist[:ngood,      -6  ]
+   nref    = tabHist[:ngood,      -5  ]
+   nsif    = tabHist[:ngood,      -4  ]
+   nvrs    = tabHist[:ngood,      -3  ]
+   nenv    = tabHist[:ngood,      -2  ]
+   ith     = tabHist[:ngood,      -1  ]
 
    NT      = XBLOCK*XGRID       # Maximum Number of threads
    NPHOTON = cd.shape[0]        # Number of photons
@@ -2016,21 +2018,22 @@ def reduce_histories(kernel2, tabHist, wl, sigma, NLOW, alb_in=None, XBLOCK=512,
    nref_in      = nref.reshape(NPHOTON,order='C').astype(np.int8)
    nvrs_in      = nvrs.reshape(NPHOTON,order='C').astype(np.int8)
    nenv_in      = nenv.reshape(NPHOTON,order='C').astype(np.int8)
+   ith_in       = ith.reshape( NPHOTON,order='C').astype(np.int8)
 
-   res_out      = gpuzeros((4, NWVL),   dtype=np.float64)
-   res_sca      = gpuzeros((4, NWVL),   dtype=np.float64)
-   res_rrs      = gpuzeros((4, NWVL),   dtype=np.float64)
-   res_sif      = gpuzeros((4, NWVL),   dtype=np.float64)
-   res_vrs      = gpuzeros((4, NWVL),   dtype=np.float64)
+   res_out      = gpuzeros((4, NWVL, NBTHETA),   dtype=np.float64)
+   res_sca      = gpuzeros((4, NWVL, NBTHETA),   dtype=np.float64)
+   res_rrs      = gpuzeros((4, NWVL, NBTHETA),   dtype=np.float64)
+   res_sif      = gpuzeros((4, NWVL, NBTHETA),   dtype=np.float64)
+   res_vrs      = gpuzeros((4, NWVL, NBTHETA),   dtype=np.float64)
 
    kernel2(np.int64(NPHOTON), np.int64(NLAYER), np.int64(NWVL), 
                      np.int64(NTHREAD), np.int64(NGROUP), np.int64(NBUNCH), 
-                     np.int64(NP_REST), np.int64(NLOW),
+                     np.int64(NP_REST), np.int64(NLOW), np.int64(NBTHETA),
                      res_out, res_sca, res_rrs, res_sif, res_vrs, 
                      to_gpu(sigma_ab_in), to_gpu(alb_in), to_gpu(cd_in),
                      to_gpu(S_in), to_gpu(weight_in), to_gpu(nrrs_in), 
                      to_gpu(nref_in), to_gpu(nsif_in), to_gpu(nvrs_in),
-                     to_gpu(nenv_in), to_gpu(iwls_in), to_gpu(wwls_in), 
+                     to_gpu(nenv_in), to_gpu(ith_in), to_gpu(iwls_in), to_gpu(wwls_in), 
                      block=(XBLOCK,1,1),grid=(XGRID,1,1))
    return res_out.get()
 
@@ -2111,9 +2114,10 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
     else : 
         tabDistTot = gpuzeros((1), dtype=np.float64)
 
-    #if hist : tabHistTot = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+5),NSENSOR,NBTHETA,NBPHI), dtype=np.float32)
+    #if hist : tabHistTot = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),NSENSOR,NBTHETA,NBPHI), dtype=np.float32)
     if hist : 
-        tabHistTot = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+5),1,1,1), dtype=np.float32)
+        #tabHistTot = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,NBTHETA,1), dtype=np.float32)
+        tabHistTot = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,1,1), dtype=np.float32)
         dz    = abs(np.diff(prof_atm.get()['z'][0,:]))
         sigma = np.diff(prof_atm.get()['OD_abs'][:,:])/dz
         # TODO add oceanic absorption
@@ -2153,8 +2157,9 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
             tabDist = gpuzeros((1), dtype=np.float32)
 
     if hist : 
-        tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+5),1,1,1), dtype=np.float32)
-        #tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+5),NSENSOR,NBTHETA,NBPHI), dtype=np.float32)
+        #tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,NBTHETA,1), dtype=np.float32)
+        tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,1,1), dtype=np.float32)
+        #tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),NSENSOR,NBTHETA,NBPHI), dtype=np.float32)
     else : 
         tabHist = gpuzeros((1), dtype=np.float32)
 
@@ -2227,7 +2232,8 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
         tabDistTot += T
         if hist :
             tabHistTot = tabHist
-            res = reduce_histories(kern2, np.squeeze(tabHist.get()), wl, sigma, NLOW)[:,None,:,None,None]
+            res = reduce_histories(kern2, np.squeeze(tabHist.get()), wl, sigma, NLOW,
+                                  NBTHETA=NBTHETA)[:,None,:,:,None]
             tabPhotonsTot[0,:,:,:,:,:] += to_gpu(res)
 
         N_simu += 1

smartg/src/device.h
@@ -133,9 +133,11 @@ __global__ void launchKernel(
 
 extern "C" {
 __global__ void reduce_absorption_gpu(unsigned long long NPHOTON, unsigned long long NLAYER, unsigned long long NWVL, 
-        unsigned long long NTHREAD, unsigned long long NGROUP, unsigned long long NBUNCH, unsigned long long NP_REST, unsigned long long NWVL_LOW,
+        unsigned long long NTHREAD, unsigned long long NGROUP, unsigned long long NBUNCH, 
+        unsigned long long NP_REST, unsigned long long NWVL_LOW, unsigned long long NBTHETA,
         double *res, double *res_sca, double *res_rrs, double *res_sif, double *res_vrs, float *ab, float *al, float *cd, float *S, float *weight, 
-        unsigned char *nrrs, unsigned char *nref, unsigned char *nsif, unsigned char *nvrs, unsigned char *nenv, unsigned char *iw_low, float *ww_low);
+        unsigned char *nrrs, unsigned char *nref, unsigned char *nsif, unsigned char *nvrs, 
+        unsigned char *nenv, unsigned char *ith, unsigned char *iw_low, float *ww_low);
 }
 
 /* initPhoton

commit 8b1c66c08d42359cbf20a6e92c7e2ae1aff44021

Correct a bug introduced in commit a6f6846fe92
- Error with GPU arch before 1xxx (Pascal)

smartg/src/device.cu
@@ -7468,12 +7468,20 @@ __global__ void reduce_absorption_gpu(unsigned long long NPHOTON, unsigned long 
         for (s=0;s<4;s++) {
             ns = s + 4*n;
             //offset = iw + NWVL*s + NWVL*NBTHETA*ith[n]; 
-            offset = ith[n] + NBTHETA*iw + NWVL*NBTHETA*s; 
+            offset = ith[n] + NBTHETA*iw + NWVL*NBTHETA*s;
+	    #if defined(DOUBLE) && !(__CUDA_ARCH__ >= 600)
+	    if (!nsif[n])             DatomicAdd(res    +offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if (!nsif[n])             DatomicAdd(res_sca+offset, (double)S[ns] *              (double)wsca * walb);
+            if ( nrrs[n] && !nsif[n]) DatomicAdd(res_rrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if ( nsif[n])             DatomicAdd(res_sif+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+            if ( nvrs[n] && !nsif[n]) DatomicAdd(res_vrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+	    #else
             if (!nsif[n])             atomicAdd(res    +offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
             if (!nsif[n])             atomicAdd(res_sca+offset, (double)S[ns] *              (double)wsca * walb);
             if ( nrrs[n] && !nsif[n]) atomicAdd(res_rrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
             if ( nsif[n])             atomicAdd(res_sif+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
             if ( nvrs[n] && !nsif[n]) atomicAdd(res_vrs+offset, (double)S[ns] * exp(-wabs) * (double)wsca * walb);
+	    #endif
         }
     }
   }