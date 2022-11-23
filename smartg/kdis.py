01743fe7129b87d101dc444ee69bc0db1fb08584
Didier Ramon <didier.ramon@gmail.com>
2022-11-23 16:37:11 +0100

    just add some new feature to kdis to_smartg() method

--- smartg/kdis.py
+++ smartg/kdis.py
@@ -401,16 +401,20 @@ class KDIS(object):
             yield self.band(i)    
             
 
-    def to_smartg(self, include='', lmin=-np.inf, lmax=np.inf):
+    def to_smartg(self, include='', lmin=-np.inf, lmax=np.inf, band_indices=None):
         '''
         return a list of KDIS_IBANDS for Smartg.run() method
         '''
         ik_l=[]
+        if band_indices is None:
+            bl = self.bands()
+        else:
+            bl = [self.band(i) for i in band_indices]
 
         if not isinstance(lmin,(list,np.ndarray)):
             lmin=[lmin]
             lmax=[lmax]
-        for k in self.bands():
+        for k in bl:
             for ii in range(len(lmin)):
                 if (k.wmin >= lmin[ii]) and (k.wmax <= lmax[ii]):
                     for ik in k.ibands():
