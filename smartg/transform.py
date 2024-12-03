Correct matrix inverse calculation with python transform multiplication

smartg/transform.py
@@ -100,7 +100,7 @@ class Transform(object):
 
     def __mul__(self, T): 
         if (isinstance(T, Transform)):
-            return Transform(np.dot(self.m, T.m), np.dot(self.mInv, T.mInv))
+            return Transform(np.dot(self.m, T.m), np.dot(T.mInv, self.mInv))
         else:
             raise NameError('mul accepted only with Transform')
 