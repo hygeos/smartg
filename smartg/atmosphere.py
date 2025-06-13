hygeos/smartg

commit 535f4fa2f75f9ddde799bbf082c86b10737603d4

minor fix for output le ('zip':True), and types changes

smartg/atmosphere.py
@@ -114,7 +114,8 @@ class AerOPAC(object):
         self._phase = phase
 
         if ssa is None : self.ssa = None
-        else           : self.ssa = np.array(ssa)
+        #else           : self.ssa = np.array(ssa)
+        else           : self.ssa = ssa
 
         if dirname(filename) == '' : self.filename = join(dir_auxdata, 'aerosols/OPAC/mixtures', filename)
         else                       : self.filename = filename
@@ -252,13 +253,22 @@ class AerOPAC(object):
         ssa[dtau!=0] /= dtau[dtau!=0]
 
         #apply scaling factor to get the required optical thickness at the
-        # specified wavelength
-        if self.tau_ref is not None: dtau *= self.tau_ref/np.sum(dtau_ref)
+        # specified wavelength or force tau for all wavelengths
+        if self.tau_ref is not None: 
+            if (isinstance(self.tau_ref, np.ndarray) and self.tau_ref.ndim == 0) or np.isscalar(self.tau_ref):
+                dtau *= self.tau_ref/np.sum(dtau_ref)
+            else:
+                assert isinstance(self.tau_ref, LUT)
+                dtau *= (self.tau_ref[Idx(wav)]/np.sum(dtau, axis=1))[:,None]
 
         # force ssa
         if self.ssa is not None:
-            if self.ssa.ndim == 0: # scalar
+            #if self.ssa.ndim == 0: # scalar
+            #if (isinstance(self.ssa, np.ndarray) and self.ssa.ndim == 0) or np.isscalar(self.ssa):
+            if False:
                 ssa[:,:] = self.ssa
+            elif isinstance(self.ssa, LUT):
+                ssa[:,:] = self.ssa[Idx(wav)][:,None]
             else:
                 ssa[:,:] = self.ssa[:,None]
 
@@ -3334,3 +3344,21 @@ def artdeco_to_smartg_cld(input_path, output_path=None, h5_group=None, normalize
     if output_path is not None: m.save(output_path, overwrite=overwrite)
 
     return m
+
+    
+def extract_split(m):
+    '''
+    Input
+        m: MLUT , result of a previous run
+        
+    extract the optical properties from a previous run and return the vertical profiles
+    prof_abs, prof_ray, prof_aer, and prof_phases the alternative inputs of the AtmAFGL
+    '''
+    pro_aer = diff1(m['OD_p'].data.astype(np.float32), axis=1)
+    ssa_aer = m['ssa_p_atm'].data
+    pro_ray = diff1(m['OD_r'].data.astype(np.float32), axis=1)
+    pro_abs = diff1(m['OD_g'].data.astype(np.float32), axis=1)
+    pro_iphase = m['iphase_atm'].data
+    pro_phases = [m['phase_atm'].sub({'iphase':i}) for i in range(pro_iphase.max()+1)]
+
+    return pro_abs, pro_ray, (pro_aer, ssa_aer), (pro_iphase, pro_phases)

environment.yml
@@ -4,25 +4,26 @@ channels:
   - conda-forge
   - defaults
 dependencies:
-  - python<3.12
-  - numpy<=1.26.4        # import numpy
-  - pandas<=2.2.0        # import pandas
+  - python
+  - numpy          # import numpy
+  - pandas               # import pandas
   - h5py
-  - pyarrow<=15.0.0
-  - openpyxl<=3.1.2      # from openpyxl import Workbook
-  - pytest<=8.0.0        # import pytest
-  - pytest-xdist<=3.5.0
-  - pytest-html<=4.1.1
-  - progressbar2<4.3     # from progressbar import ProgressBar, Percentage, Bar
-  - matplotlib<=3.8.3    # import matplotlib
-  - pycuda<=2024.1       # import pycuda
+  - pyarrow
+  - openpyxl      # from openpyxl import Workbook
+  - pytest      # import pytest
+  - pytest-xdist
+  - pytest-html
+  - progressbar2     # from progressbar import ProgressBar, Percentage, Bar
+  - matplotlib    # import matplotlib
+  - pycuda               # import pycuda
   - papermill
-  - jupyterlab<=4.1.1
-  - notebook<=7.1.0
-  - ipywidgets<=8.1.2
-  - ipympl<=0.9.3
-  - pyhdf<=0.11.3
-  - pip<=24.0
+  - jupyterlab
+  - notebook
+  - ipywidgets
+  - ipympl
+  - pyhdf
+  - pip
+  - jax
   - pip:
     - luts
     - hitran-api

smartg/smartg.py
@@ -38,6 +38,8 @@ from smartg.transform import Transform, Aff
 from smartg.visualizegeo import Mirror, Plane, Spheric, Transformation, \
     Entity, Analyse_create_entity, LambMirror, Matte, convertVtoAngles, \
     convertAnglestoV, GroupE
+    
+import functools
 
 
 # set up directories
@@ -543,6 +545,25 @@ class CusBackward(object):
     def __str__(self):
         return 'CusBackward:-POS={POS}-THDEG={THDEG}-PHDEG={PHDEG}'.format(**self.dict) + \
             '-ALDEG={ALDEG}-TYPE{TYPE}-LMODE={LMODE}'.format(**self.dict)
+            
+
+def with_cuda_context ( func ) :
+    """
+    A decorator that creates a CUDA context before calling
+    a given function ’func ’ and then cleans the context .
+    """
+    @functools.wraps ( func )
+    def wrapper(*args , **kwargs ):
+        cuda.init()
+        ctx = cuda.Device(0).make_context()
+        try :
+            result = func(*args ,**kwargs)
+        finally :
+            ctx.pop()
+            ctx.detach()
+        return result
+    return wrapper
+    
     
 class Smartg(object):
     '''Initialization of the Smartg object
@@ -592,7 +613,7 @@ class Smartg(object):
                 * CURAND_PHILOX
 
     '''
-    def __init__(self, pp=True, debug=False,
+    def __init__(self, pp=True, debug=False, autoinit=False,
                  verbose_photon=False,
                  double=True, alis=False, back=False, bias=True, alt_pp=False, obj3D=False, 
                  opt3D=False, device=None, sif=False, thermal=False, rng='PHILOX', cache_dir='/tmp/'):
@@ -602,8 +623,17 @@ class Smartg(object):
             env_modif = {'CUDA_DEVICE': str(device)}
         else:
             env_modif = {}
-        with modified_environ(**env_modif):
-            import pycuda.autoinit
+        #with modified_environ(**env_modif):
+        #    import pycuda.autoinit
+            
+        if (autoinit):
+            with modified_environ(**env_modif):
+                import pycuda.autoinit
+        else:
+            import pycuda
+            import pycuda.driver as cuda
+            cuda.init()
+            self.ctx = cuda.Device(0).make_context()
 
         self.pp = pp
         self.double = double
@@ -693,11 +723,18 @@ class Smartg(object):
         self.common_attrs = OrderedDict()
         self.common_attrs['compilation_time'] = (datetime.now()
                         - time_before_compilation).total_seconds()
-        self.common_attrs['device'] = pycuda.autoinit.device.name()
-        try:
-            self.common_attrs['device_number'] = pycuda.autoinit.device.get_attributes()[pycuda._driver.device_attribute.MULTI_GPU_BOARD_GROUP_ID]
-        except AttributeError:
-            self.common_attrs['device_number'] = 'undefined'
+        if (autoinit):
+            self.common_attrs['device'] = pycuda.autoinit.device.name()
+            try:
+                self.common_attrs['device_number'] = pycuda.autoinit.device.get_attributes()[pycuda._driver.device_attribute.MULTI_GPU_BOARD_GROUP_ID]
+            except AttributeError:
+                self.common_attrs['device_number'] = 'undefined'
+        else:
+            self.common_attrs['device'] =self.ctx.get_device().name()
+            try:
+                self.common_attrs['device_number'] = self.ctx.getçdevice().get_attributes()[pycuda._driver.device_attribute.MULTI_GPU_BOARD_GROUP_ID]
+            except Exception:
+                self.common_attrs['device_number'] = 'undefined'
         self.common_attrs['pycuda_version'] = pycuda.VERSION_TEXT
         self.common_attrs['cuda_version'] = '.'.join([str(x) for x in pycuda.driver.get_version()])
         self.common_attrs.update(get_git_attrs())
@@ -944,7 +981,8 @@ class Smartg(object):
             if (surfLPH_RF is not None): surfLPH = surfLPH_RF
 
         else:
-            myObjects0 = gpuzeros(1, dtype='int32')
+            myObjects0 = gpuzeros(1, dtype=np.uint32)
+            #myObjects0 = gpuzeros(1, dtype='int32')
             myGObj0 = gpuzeros(1, dtype='int32')
             myRObj0 = gpuzeros(1, dtype='int32')
             mySPECTObj0 = gpuzeros(1, dtype='int32') # normally 2 dims: obj dim + wl dim
@@ -1356,6 +1394,9 @@ class Smartg(object):
         if wl.scalar:
             output = output.dropaxis('wavelength')
             output.attrs['wavelength'] = wl[:]
+        
+
+
 
         return output
 
@@ -1411,6 +1452,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         if le!=None : 
             tabTh = le['th']
             tabPhi = le['phi']
+            if 'zip' not in le.keys():
+                zip = False
+            else : zip = le['zip']
             norm_geo =  1. 
         else : 
             tabTh, tabPhi, tabOmega = calcOmega(NBTHETA, NBPHI, SZA_MAX=SZA_MAX, SUN_DISC=SUN_DISC)
@@ -1499,7 +1543,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
         m.add_dataset('U_stdev_up (TOA)', sigma[UPTOA,2,isen,ilam,iphi,:], axnames)
         m.add_dataset('V_stdev_up (TOA)', sigma[UPTOA,3,isen,ilam,iphi,:], axnames)
     m.add_dataset('N_up (TOA)', NPhotonsOutTot[UPTOA,isen,ilam,iphi,:], axnames)
-    if len(tabDistFinal) > 1: m.add_dataset('cdist_up (TOA)', tabDistFinal[UPTOA,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+    if len(tabDistFinal) > 1: 
+        if zip : m.add_dataset('cdist_up (TOA)', np.squeeze(tabDistFinal[UPTOA,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+        else   : m.add_dataset('cdist_up (TOA)', tabDistFinal[UPTOA,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
     
     if hist : m.add_dataset('histories', tabHistTot)
     
@@ -1514,7 +1560,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
             m.add_dataset('U_stdev_down (0+)', sigma[DOWN0P,2,isen,ilam,iphi,:], axnames)
             m.add_dataset('V_stdev_down (0+)', sigma[DOWN0P,3,isen,ilam,iphi,:], axnames)
         m.add_dataset('N_down (0+)', NPhotonsOutTot[DOWN0P,isen,ilam,iphi,:], axnames)
-        if len(tabDistFinal) > 1: m.add_dataset('cdist_down (0+)', tabDistFinal[DOWN0P,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+        if len(tabDistFinal) > 1: 
+            if zip : m.add_dataset('cdist_down (0+)', np.squeeze(tabDistFinal[DOWN0P,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+            else   : m.add_dataset('cdist_down (0+)', tabDistFinal[DOWN0P,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
     
         m.add_dataset('I_up (0-)', tabFinal[UP0M,0,isen,ilam,iphi,:], axnames)
         m.add_dataset('Q_up (0-)', tabFinal[UP0M,1,isen,ilam,iphi,:], axnames)
@@ -1526,7 +1574,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
             m.add_dataset('U_stdev_up (0-)', sigma[UP0M,2,isen,ilam,iphi,:], axnames)
             m.add_dataset('V_stdev_up (0-)', sigma[UP0M,3,isen,ilam,iphi,:], axnames)
         m.add_dataset('N_up (0-)', NPhotonsOutTot[UP0M,isen,ilam,iphi,:], axnames)
-        if len(tabDistFinal) > 1: m.add_dataset('cdist_up (0-)', tabDistFinal[UP0M,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+        if len(tabDistFinal) > 1: 
+            if zip : m.add_dataset('cdist_up (0-)', np.squeeze(tabDistFinal[UP0M,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+            else   : m.add_dataset('cdist_up (0-)', tabDistFinal[UP0M,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
 
     if OUTPUT_LAYERS & 2:
         m.add_dataset('I_down (0-)', tabFinal[DOWN0M,0,isen,ilam,iphi,:], axnames)
@@ -1539,7 +1589,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
             m.add_dataset('U_stdev_down (0-)', sigma[DOWN0M,2,isen,ilam,iphi,:], axnames)
             m.add_dataset('V_stdev_down (0-)', sigma[DOWN0M,3,isen,ilam,iphi,:], axnames)
         m.add_dataset('N_down (0-)', NPhotonsOutTot[DOWN0M,isen,ilam,iphi,:], axnames)
-        if len(tabDistFinal) > 1: m.add_dataset('cdist_down (0-)', tabDistFinal[DOWN0M,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+        if len(tabDistFinal) > 1: 
+            if zip : m.add_dataset('cdist_down (0-)', np.squeeze(tabDistFinal[DOWN0M,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+            else   : m.add_dataset('cdist_down (0-)', tabDistFinal[DOWN0M,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
 
         m.add_dataset('I_up (0+)', tabFinal[UP0P,0,isen,ilam,iphi,:], axnames)
         m.add_dataset('Q_up (0+)', tabFinal[UP0P,1,isen,ilam,iphi,:], axnames)
@@ -1551,7 +1603,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
             m.add_dataset('U_stdev_up (0+)', sigma[UP0P,2,isen,ilam,iphi,:], axnames)
             m.add_dataset('V_stdev_up (0+)', sigma[UP0P,3,isen,ilam,iphi,:], axnames)
         m.add_dataset('N_up (0+)', NPhotonsOutTot[UP0P,isen,ilam,iphi,:], axnames)
-        if len(tabDistFinal) > 1: m.add_dataset('cdist_up (0+)', tabDistFinal[UP0P,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+        if len(tabDistFinal) > 1: 
+            if zip : m.add_dataset('cdist_up (0+)', np.squeeze(tabDistFinal[UP0P,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+            else   : m.add_dataset('cdist_up (0+)', tabDistFinal[UP0P,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
 
         m.add_dataset('I_down (B)', tabFinal[DOWNB,0,isen,ilam,iphi,:], axnames)
         m.add_dataset('Q_down (B)', tabFinal[DOWNB,1,isen,ilam,iphi,:], axnames)
@@ -1563,7 +1617,9 @@ def finalize(tabPhotonsTot, tabDistTot, tabHistTot, wl, NPhotonsInTot, errorcoun
             m.add_dataset('U_stdev_down (B)', sigma[DOWNB,2,isen,ilam,iphi,:], axnames)
             m.add_dataset('V_stdev_down (B)', sigma[DOWNB,3,isen,ilam,iphi,:], axnames)
         m.add_dataset('N_down (B)', NPhotonsOutTot[DOWNB,isen,ilam,iphi,:], axnames)
-        if len(tabDistFinal) > 1: m.add_dataset('cdist_down (B)', tabDistFinal[DOWNB,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
+        if len(tabDistFinal) > 1: 
+            if zip : m.add_dataset('cdist_down (B', np.squeeze(tabDistFinal[DOWNB,:,isen,:,:]),  ['None','Zenith angles','iAMF'])
+            else   : m.add_dataset('cdist_down (B)', tabDistFinal[DOWNB,:,isen,:,:,:],['None','Azimuth angles','Zenith angles','iAMF'])
 
 
     # write atmospheric profiles
@@ -2434,8 +2490,9 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
 
     """
     # Initializations
-    nThreadsActive = gpuzeros(1, dtype='int32')
-    Counter = gpuzeros(1, dtype='uint64')
+    nThreadsActive = gpuzeros(1, dtype=np.uint32)
+    Counter = gpuzeros(1, dtype=np.uint64)
+
     
     if double : FDTYPE=np.float64
     else : FDTYPE=np.float32
@@ -2529,6 +2586,7 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
         #tabHist = gpuzeros((MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,NBTHETA,1), dtype=np.float32)
         #tabHist = gpuzeros((2,MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),1,1,1), dtype=np.float32)
         tabHistTot = gpuzeros((2,MAX_HIST,(NATM_ABS+NOCE_ABS+NPSTK+NLOW+6),NSENSOR,NBTHETA,NBPHI), dtype=np.float32)
+
     else : 
         tabHistTot = gpuzeros((1), dtype=np.float32)
 
@@ -2723,8 +2781,33 @@ def loop_kernel(NBPHOTONS, faer, foce, NLVL, NATM, NATM_ABS, NOCE, NOCE_ABS, MAX
     else:
         sigma = None
 
-    return NPhotonsInTot.get(), tabPhotonsTot.get(), tabDistTot.get(), tabHistTot.get(), tabTransDir.get(), errorcount, \
-        NPhotonsOutTot.get(), sigma, N_simu, secs_cuda_clock, tabMatRecep, matCats, matLoss, wPhCatTot.get(), wPhCat2Tot.get()
+    NPhotonsInTot_VAL = NPhotonsInTot.get()
+    tabPhotonsTot_VAL = tabPhotonsTot.get()
+    tabDistTot_VAL    = tabDistTot.get()
+    tabHistTot_VAL    = tabHistTot.get()
+    tabTransDir_VAL   = tabTransDir.get()
+    NPhotonsOutTot_VAL= NPhotonsOutTot.get()
+    wPhCatTot_VAL     = wPhCatTot.get()
+    wPhCat2Tot_VAL    = wPhCat2Tot.get()
+    del NPhotonsInTot
+    del tabPhotonsTot
+    del tabDistTot
+    del tabTransDir
+    del NPhotonsOutTot
+    del wPhCatTot
+    del wPhCat2Tot
+    del NPhotonsIn
+    del tabPhotons
+    del tabDist
+    del NPhotonsOut
+    del wPhCat
+    del wPhCat2
+    
+    
+    return NPhotonsInTot_VAL, tabPhotonsTot_VAL, tabDistTot_VAL, tabHistTot_VAL, tabTransDir_VAL, errorcount, \
+        NPhotonsOutTot_VAL, sigma, N_simu, secs_cuda_clock, tabMatRecep, matCats, matLoss, wPhCatTot_VAL, wPhCat2Tot_VAL
+    #return NPhotonsInTot.get(), tabPhotonsTot.get(), tabDistTot.get(), tabHistTot_TEST, tabTransDir.get(), errorcount, \
+        #NPhotonsOutTot.get(), sigma, N_simu, secs_cuda_clock, tabMatRecep, matCats, matLoss, wPhCatTot.get(), wPhCat2Tot.get()
 
 
 def get_git_attrs():
@@ -2915,7 +2998,7 @@ class RNG_CURAND_PHILOX(object):
         self.mod = SourceModule(source, no_extern_c=True)
 
         # get state size
-        s = gpuzeros(1, dtype='int32')
+        s = gpuzeros(1, dtype=np.uint32)
         self.mod.get_function('get_state_size')(s, block=(1, 1, 1), grid=(1, 1, 1))
         self.STATE_SIZE = int(np.squeeze(s.get()))  # size in bytes
 

smartg/validation/HYDROLIGHT/pure_water.dat
@@ -1361,7 +1361,7 @@
 1.360000000000000000e+02 8.914639110711256031e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
 1.360999999999999943e+02 8.923705016580397997e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
 1.361999999999999886e+02 8.932769706813831201e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
-1.363000000000000114e+02 8.941833070961172081e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
+1.363000000000000114e+02 8.941833070961173469e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
 1.364000000000000057e+02 8.950894998588188045e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
 1.365000000000000000e+02 8.959955379278146392e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00
 1.365999999999999943e+02 8.969014102633170171e-02 0.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00

tests/test_smartg.py
@@ -38,7 +38,7 @@ def test_compile(pp, back):
 
 def test_basic(request):
     ''' Most basic test '''
-    m = Smartg().run(500., atm=AtmAFGL('afglms'), NBPHOTONS=NBPHOTONS)
+    m = Smartg(autoinit=True).run(500., atm=AtmAFGL('afglms'), NBPHOTONS=NBPHOTONS)
     smartg_view(m)
     conftest.savefig(request)
 

commit 779a0dbd649df5824485d71d83c21eb03eb6a158

Correct bug introduced in commit 535f4fa, and some improvements

smartg/atmosphere.py
@@ -52,8 +52,8 @@ class AerOPAC(object):
         Force min and max altitude of free troposphere
     H_stra_min/max : float, optional
         Force min and max altitude of stratosphere
-    ssa : float | np.ndarray
-        Force particle single scattering albedo (scalar or 1-d array-like for multichromatic)
+    ssa : float | list | 1-D ndarray | 2-D ndarray | LUT
+        Force particle single scattering albedo. If a list is given, it will be converted into an ndarray.
     phase : luts.LUT, optional
         Phase matrix F as function of humidity, wavelength, stoke components and scattering angle    
         The variable names must be: hum (humidity), wav (wavelength), stk (stoke components) and  
@@ -115,8 +115,15 @@ class AerOPAC(object):
         self._phase = phase
 
         if ssa is None : self.ssa = None
-        #else           : self.ssa = np.array(ssa)
-        else           : self.ssa = ssa
+        else           :
+            if (isinstance(ssa, list)) :
+                ssa = np.array(ssa)
+            if ( np.isscalar(ssa)                                 or
+                 (isinstance(ssa, np.ndarray) and (ssa.ndim <=2)) or
+                 isinstance(ssa, LUT) ):
+                self.ssa = ssa
+            else:
+                raise ValueError ("The ssa variable must a scalar, a list, an ndarray of dim <= 2, or a LUT.")
 
         if dirname(filename) == '' : self.filename = join(dir_auxdata, 'aerosols/OPAC/mixtures', filename)
         else                       : self.filename = filename
@@ -264,15 +271,14 @@ class AerOPAC(object):
 
         # force ssa
         if self.ssa is not None:
-            #if self.ssa.ndim == 0: # scalar
-            #if (isinstance(self.ssa, np.ndarray) and self.ssa.ndim == 0) or np.isscalar(self.ssa):
-            if False:
-                ssa[:,:] = self.ssa
-            elif isinstance(self.ssa, LUT):
-                ssa[:,:] = self.ssa[Idx(wav)][:,None]
-            else:
-                ssa[:,:] = self.ssa[:,None]
-
+            if np.isscalar(self.ssa): # scalar
+                ssa[:,:] = float(self.ssa)
+            elif isinstance(self.ssa, np.ndarray): # ndarray with dim <= 2
+                if self.ssa.ndim == 0: ssa[:,:] = self.ssa
+                elif self.ssa.ndim == 1: ssa[:,:] = self.ssa[:,None] # If 1d array -> wl considered constant
+                elif self.ssa.ndim == 2: ssa[:,:] = self.ssa[:,:]
+            else: # LUT
+                ssa[:,:] = self.ssa[Idx(wav)][:,None] # introduced by DR. Why only considering 1 wl?
         return dtau, ssa
     
     