275a0f5cbc42b4a12f68f690330c85b6e2734de2
Mustapha Moulana <mm@hygeos.com>
2025-06-17 17:48:18 +0200

    Allow also old way for auxdata

--- smartg/config.py
+++ smartg/config.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os.path import join, dirname, realpath
-from pathlib import Path
-from textwrap import dedent
-from core import env, log
+from os import environ
+from os.path import join, dirname, realpath, isdir
+from dotenv import load_dotenv
+import warnings
 
 '''
 SMART-G constant variables
@@ -13,17 +13,26 @@ SMART-G constant variables
 
 dir_root = dirname(dirname(realpath(__file__)))
 NPSTK = 4
-# dir_auxdata = join(dir_root, 'auxdata')
-dir_auxdata = env.getdir("SMARTG_DIR_DATA", Path("auxdata"))
+dir_auxdata_old = join(dir_root, 'auxdata')
 
-if not dir_auxdata.is_dir():
-    log.error(dedent(
-        """Missing auxdata dir in environment variables (SMARTG_DIR_DATA)
-        define the environment variable SMARTG_DIR_DATA
-        and run $ smartg download_auxdata
-        to download the require ancillary data.
-        """), e=EnvironmentError)
+load_dotenv(dir_root) # To consider .env file
 
-# Deprecated, will be removed in one of the next release
+# Allow old way in v1.1, but remove it in next releases
+try:
+    dir_auxdata_new = environ['SMARTG_DIR_AUXDATA']
+    dir_auxdata = dir_auxdata_new
+except KeyError:
+    if not (isdir(dir_auxdata_old)):
+        raise NameError("The environment variable 'SMARTG_DIR_AUXDATA' does not exist!")
+    else:
+        warnings.simplefilter('always', DeprecationWarning)
+        warn_message = "\nThe 'SMARTG_DIR_AUXDATA' variable does not exist, but the folder auxdata has \n" + \
+                       "been found in the root directory and is used instead. This is deprecated, and will \n" + \
+                       "not be allowed in next releases. Please give the auxdata directory path by \n" + \
+                       "using the environment variable 'SMARTG_DIR_AUXDATA' instead."
+        warnings.warn(warn_message, DeprecationWarning)
+        dir_auxdata = dir_auxdata_old
+
+# TODO for constant variable must use uppercase 
 dir_libradtran = join(dir_auxdata, 'libRadtran')
 dir_libradtran_atmmod = join(dir_libradtran, 'data/atmmod/')
