ff177e34932c0ddcf1a6517f03700536eba8e3cc
Mustapha Moulana <mm@hygeos.com>
2025-06-21 18:38:16 +0200

    Update test_notebook and pixi

--- smartg/tests/test_notebook.py
+++ smartg/tests/test_notebook.py
@@ -9,7 +9,7 @@ def test_demo_notebook():
     Execute the demo notebook
     """
     print("\nTesting demo_notebook.ipynb...")
-    nb_path = join(ROOTPATH, "notebooks/demo_notebook.ipynb")
+    nb_path = join(ROOTPATH, "smartg/notebooks/demo_notebook.ipynb")
     nb_output_path = join(ROOTPATH, "smartg/tests/logs/demo_notebook_log.ipynb")
     pm.execute_notebook(str(nb_path), str(nb_output_path), cwd=ROOTPATH)
 
@@ -18,6 +18,6 @@ def test_demo_notebook_objects():
     Execute the demo notebook objects
     """
     print("\nTesting demo_notebook_objects.ipynb...")
-    nb_path = join(ROOTPATH, "notebooks/demo_notebook_objects.ipynb")
+    nb_path = join(ROOTPATH, "smartg/notebooks/demo_notebook_objects.ipynb")
     nb_output_path = join(ROOTPATH, "smartg/tests/logs/demo_notebook_objects_log.ipynb")
     pm.execute_notebook(str(nb_path), str(nb_output_path), cwd=ROOTPATH)
