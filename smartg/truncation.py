f19157e0e050553ce5e79c2e51d95b14e5f0bee5
Mustapha Moulana <mm@hygeos.com>
2026-01-27 15:15:18 +0100

    Correct docstring in truncation.py

--- smartg/truncation.py
+++ smartg/truncation.py
@@ -12,14 +12,14 @@ class DM_trunc(object):
     integral_method : str, optional
         Integration method to use for computing the moments. Choices are:
 
-        - 'lobatto' : use Lobatto quadrature (default)
-        - 'trapezoidal' : use scypi.integrate.trapezoid method
-        - 'simpson' : use scipy.integrate.simpson method
+        - 'lobatto' -> use Lobatto quadrature (default)
+        - 'trapezoid' -> use scypi.integrate.trapezoid method
+        - 'simpson' -> use scipy.integrate.simpson method
     pha_scale_method : int, optional
         Scaling method to use for the truncated phase matrix. Choices are:
 
-        - 1 : use Eq. 5 in Waquet et al. 2019 (Default)
-        - 2 : use ARTDECO way, (same as 1, but with diffferent rescalling for F21 and F34)
+        - 1 -> use Eq. 5 in Waquet et al. 2019 (Default)
+        - 2 -> use ARTDECO way, (same as 1, but with diffferent rescalling for F21 and F34)
     """
     def __init__(self, nb_streams, integral_method='lobatto', pha_scale_method=1):
         # check parameter values
@@ -49,9 +49,9 @@ class GT_trunc(object):
     integral_method : str, optional
         Integration method to use for computing the moments. Choices are:
 
-        - 'lobatto' : use Lobatto quadrature (default)
-        - 'trapezoid' : use scypi.integrate.trapezoid method
-        - 'simpson' : use scipy.integrate.simpson method
+        - 'lobatto' ->  use Lobatto quadrature (default)
+        - 'trapezoid' ->  use scypi.integrate.trapezoid method
+        - 'simpson' ->  use scipy.integrate.simpson method
     theta_tol : None | float, optional
         Search the truncated angle between 0 and theta_tol (in degrees). 
     theta_tr : None | float, optional
@@ -63,8 +63,8 @@ class GT_trunc(object):
     pha_scale_method : int, optional
         Scaling method to use for the truncated phase matrix. Choices are:
 
-        - 1 : use Eq. 5 in Waquet et al. 2019 (Default)
-        - 2 : use ARTDECO way, (same as 1, but with diffferent rescalling for F12 and F34)
+        - 1 -> use Eq. 5 in Waquet et al. 2019 (Default)
+        - 2 -> use ARTDECO way, (same as 1, but with diffferent rescalling for F12 and F34)
     """
     def __init__(self, trunc_frac, integral_method='lobatto', theta_tol=None, theta_tr=None, 
                  lobatto_optimization=False, pha_scale_method=1):
